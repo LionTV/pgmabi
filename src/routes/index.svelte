7a21c8ca910f139815d999677aae7d4d6de9d62e
Leon <81768885+LionTV@users.noreply.github.com>
2022-09-08 13:18:47 +0200

    remove useless modal

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,29 +38,6 @@ import { browser } from "$app/env";
   <Footer />
 </div>
 
-{#if showModal}
-  <Modal on:close="{() => showModal = false}">
-    <h1 slot="header" class="text-white font-bold text-xl">Settings</h1>
-    <div class="flex justify-between">
-      <p class="text-white font-semibold mt-2">Darkmode</p>
-      <div class="mt-2 mb-4">
-        <label for="toggleB" class="flex items-center cursor-pointer">
-          <div class="relative">
-            {#if darkmode === true}
-              <input checked on:click="{() => toggle()}" type="checkbox" id="toggleB" class="sr-only">
-                <div class="block bg-slate-500 w-14 h-8 rounded-full"></div>
-              <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-            {:else}
-              <input on:click="{() => toggle()}" type="checkbox" id="toggleB" class="sr-only">
-                <div class="block bg-slate-500 w-14 h-8 rounded-full"></div>
-              <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-            {/if}
-            </div>
-        </label>
-      </div>
-    </div>
-  </Modal>
-{/if}
 
 <style>
   :global(body.light-mode) {
